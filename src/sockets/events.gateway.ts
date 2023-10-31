72995589a34270386f0dffbd9a53e6f9a726dcd6
anishabid-fandist <100930746+anishabid-fandist@users.noreply.github.com>
2023-10-31 15:09:26 +0500

    socket added

--- src/sockets/events.gateway.ts
+++ src/sockets/events.gateway.ts
@@ -4,15 +4,51 @@ import {
   SubscribeMessage,
   WebSocketGateway,
   WsException,
+  WebSocketServer
 } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 import { RequestInterceptor } from './request.interceptor';
 import { throwError } from 'rxjs';
 import { RequestFilter } from './request.filter';
 
 @WebSocketGateway()
 export class ApplicationGateway {
+    @WebSocketServer()
+    private server: Server;
+    private io: Socket;
+    private clients=[];
+    afterInit(server: Server) {
+        console.log('Initialized!');
+    }
+    handleConnection(client: Socket, ...args: any[]) {
+        console.log(`Client connected: ${client.id}`);
+        this.clients.push(client.id)
+        console.log("Clients List",this.clients)
+    }
+    handleDisconnect(client: Socket) {
+        console.log(`Client disconnected: ${client.id}`);
+        let index = this.clients.indexOf(client.id);
+        this.clients.splice(index, 1);
+    }
+
+    public handleBroadCast(data:string){
+        console.log(`Boradcast called`);
+        this.server.emit("client",data)
+        //("client","Hono lolo hono cactus"+data+"")
+    }
+
+    @SubscribeMessage('joinRoom')
+    private onJoinRoom(client, data){
+    
+    }
+
   @SubscribeMessage('push')
-  onPush(@MessageBody() data) {
+  private onPush(client, data) {
+    /* 
+    Direct emit event to the client by using @ConnectedSocket() client or with just client.
+    */
+    client.emit('client',"Pakore Lag gay")
+
     return {
       event: 'pop',
       data,
@@ -21,7 +57,7 @@ export class ApplicationGateway {
 
   @UseInterceptors(RequestInterceptor)
   @SubscribeMessage('getClient')
-  getPathCalled(client, data) {
+  private getPathCalled(client, data) {
     return {
       event: 'popClient',
       data: { ...data, path: client.pattern },
@@ -30,7 +66,7 @@ export class ApplicationGateway {
 
   @UseFilters(RequestFilter)
   @SubscribeMessage('getClientWithError')
-  getPathCalledWithError() {
+  private getPathCalledWithError() {
     return throwError(() => new WsException('This is an error'));
   }
 }
